bcec385f47352cb5b500e6c98cd5a72f4a5dcae3
Manny Pissanos <mpissano@gmail.com>
2018-10-07 18:57:21 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,15 +18,8 @@ class Artist
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
-    
-    
+    song.artist = self
   end
 
 end
 
-
- def add_song_by_name(name, genre)
-    song = Song.new(name, genre)
-    @songs << song
-    song.artist = self
-  end

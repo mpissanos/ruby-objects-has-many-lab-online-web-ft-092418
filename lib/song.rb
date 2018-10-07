9bd0aad7d431b9a39555390e7f7946ef3af493e1
Manny Pissanos <mpissano@gmail.com>
2018-10-07 19:25:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,14 @@
 class Song
   attr_accessor :artist, :name, :genre
  
-  # @@all_songs = []
+  @@all_songs = []
   
   def initialize(name)
     @name = name
     # @genre = genre
   end
   
-  # def self.all_songs
-  #   @@all_songs
-  # end
+  def self.all_songs
+    @@all_songs
+  end
 end

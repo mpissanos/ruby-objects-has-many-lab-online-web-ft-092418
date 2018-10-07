470d95cd0ba9beecb6a98f015db84c8c98a64cba
Manny Pissanos <mpissano@gmail.com>
2018-10-07 19:00:14 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,42 +1,25 @@
-# class Artist
-#   attr_accessor :name, :song
-  
-#   def initialize(name)
-#     @songs = []
-#     @name = name
-#   end
-  
-#   def songs
-#     @songs
-#   end
-  
-#   def add_song(song)
-#     @songs << song
-#     song.artist = self
-#   end
-  
-#   def add_song_by_name(name)
-#     song = Song.new(name)
-#     @songs << song
-#     song.artist = self
-#   end
-
-# end
-
 class Artist
-  attr_accessor :name
- 
+  attr_accessor :name, :song
+  
   def initialize(name)
-    @name = name
     @songs = []
+    @name = name
   end
- 
+  
+  def songs
+    @songs
+  end
+  
   def add_song(song)
     @songs << song
     song.artist = self
   end
- 
-  def songs
-    @songs
+  
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
   end
+
 end
+
